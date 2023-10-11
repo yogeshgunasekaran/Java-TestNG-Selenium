0ebdc38f2284011f082982de5d5beb8c327e3b72
Ravinder-Yogesh <ravinder@techgroov.com>
2023-10-11 17:03:05 +0530

    imported web chrom

--- src/test/java/com/lambdatest/TestNGTodo2.java
+++ src/test/java/com/lambdatest/TestNGTodo2.java
@@ -13,6 +13,7 @@ import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
 
 public class TestNGTodo2 {
 
